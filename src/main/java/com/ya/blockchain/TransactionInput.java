fdf67120afb8807a843a17dd7995a55ee2474977
yass-arafat <roni.yassir@gmail.com>
2018-11-10 12:33:48 +0600

    Initial commit

---
+++ src/main/java/com/ya/blockchain/TransactionInput.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ya.blockchain;
+
+/**
+ *
+ * @author yassir
+ */
+public class TransactionInput {
+
+    public String transactionOutputId; //Reference to TransactionOutputs -> transactionId
+    public TransactionOutput UTXO; //Contains the Unspent transaction output
+
+    public TransactionInput(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+    }
+}
